ce41bf69c7e28dff7c0d43d1fe9b2f119107652e
Павел Струначев <pavelstrunacev@iMac-Pavel.local>
2023-07-26 22:25:57 +0500

    продолжение верстки

--- mascara/index.php
+++ mascara/index.php
@@ -18,37 +18,203 @@ require '../mascara/templates/head.html';
 <!---------------------BODY---------------------->
 
 <body>
-  
-<div class="banner-box">
-<div class="banner-one">
-  <h1 class="banner-h1">Наращивание ресниц Тюмень</h1>
-  <h2 class="banner-h2">500 рублей на первое наращивание</h2>
-  <div class="banner--btnonline"><ion-icon class="banner--btnonline--arrow" name="chevron-down-outline"></ion-icon>Подробнее</div>
-</div>
-  <div class="banner-two">
-    <div class="banner--slider-post">
-      <button>123</button>
-      <button>123</button>
-      <button>123</button>
+  <!----------------------------------------------->
+  <div class="banner-box">
+    <div class="banner-one">
+      <h1 class="banner-h1">Наращивание ресниц Тюмень</h1>
+      <h2 class="banner-h2">500 рублей на первое наращивание</h2>
+      <div class="btn"><ion-icon class="banner--btnonline--arrow" name="chevron-down-outline"></ion-icon>Подробнее</div>
+    </div>
+    <div class="banner-two">
+      <div class="banner--slider-post">
+        <button>123</button>
+        <button>123</button>
+        <button>123</button>
+      </div>
     </div>
   </div>
-</div>
-
-<div class="advantages-box">
-  <h2 class="advantages--h2">Взгляд внушающий уверенность</h2>
-  <div class="advantages--img">
-    <div class="advantages--img--item">
-    <p>Правильное разделение</p>
-    </div>
-    <div class="advantages--img--item">
-    <p>Подчеркиваем красоту</p>
-    </div>
-    <div class="advantages--img--item">
-    <p>Меняем архитектуру глаз</p>
+  <!----------------------------------------------->
+  <div class="advantages-box container">
+    <h2 class="h2-item">Взгляд внушающий уверенность</h2>
+    <div class="advantages--img">
+      <div class="advantages--img--item">
+        <div class="advantages--img--container">
+          <p>Правильное разделение и направление ресниц</p>
+        </div>
+      </div>
+      <div class="advantages--img--item">
+        <div class="advantages--img--container">
+          <p>Подчеркиваем красоту</p>
+        </div>
+      </div>
+      <div class="advantages--img--item">
+        <div class="advantages--img--container">
+          <p>Меняем архитектуру глаз</p>
+        </div>
+      </div>
     </div>
   </div>
-</div>
+  <!----------------------------------------------->
+  <div class="black-box container">
+    <div class="black-box-section--one">
+      <p class="black-box--title">A new perspective on your life</p>
+    </div>
+    <div class="black-box-section--two">
+      <p class="black-box--font">РЫБИЙ ТЕКСТ РАССКАЗЫВАЮЩИЙ ПРО НАРАЩИВАНИЕ РЕСНИЦ. РЫБИЙ ТЕКСТ РАССКАЗЫВАЮЩИЙ ПРО НАРАЩИВАНИЕ РЕСНИЦ. РЫБИЙ ТЕКСТ РАССКАЗЫВАЮЩИЙ ПРО НАРАЩИВАНИЕ РЕСНИЦ. РЫБИЙ ТЕКСТ РАССКАЗЫВАЮЩИЙ ПРО НАРАЩИВАНИЕ РЕСНИЦ. РЫБИЙ ТЕКСТ РАССКАЗЫВАЮЩИЙ ПРО НАРАЩИВАНИЕ РЕСНИЦ. РЫБИЙ ТЕКСТ РАССКАЗЫВАЮЩИЙ ПРО НАРАЩИВАНИЕ РЕСНИЦ. </p>
+    </div>
+  </div>
+  <!----------------------------------------------->
+  <div class="servises-box">
+    <div class="servises--item--left">
+      <div class="servises--item--title">
+        <h3>Классическое наращивание</h3>
+      </div>
+      <div class="servises--item--descript">
+        <ul>
+          <li>Рыбий текст в чем преимущества</li>
+          <li>Рыбий текст в чем преимущества</li>
+          <li>Рыбий текст в чем преимущества</li>
+          <li>Рыбий текст в чем преимущества</li>
+        </ul>
+      </div>
+      <div class="servises--btn btn">Подробнее</div>
+    </div>
+    <div class="servises--item--right">
+      <div class="servises--item--img"></div>
+    </div>
+  </div>
 
+  <div class="servises-box">
+    <div class="servises--item--left">
+      <div class="servises--item--title">
+        <h3>Наращивание 2D</h3>
+      </div>
+      <div class="servises--item--descript">
+        <ul>
+          <li>Рыбий текст в чем преимущества</li>
+          <li>Рыбий текст в чем преимущества</li>
+          <li>Рыбий текст в чем преимущества</li>
+          <li>Рыбий текст в чем преимущества</li>
+        </ul>
+      </div>
+      <div class="servises--btn btn">Подробнее</div>
+    </div>
+    <div class="servises--item--right">
+      <div class="servises--item--img"></div>
+    </div>
+  </div>
+
+  <div class="servises-box">
+    <div class="servises--item--left">
+      <div class="servises--item--title">
+        <h3>Мокрое наращивание ресниц</h3>
+      </div>
+      <div class="servises--item--descript">
+        <ul>
+          <li>Рыбий текст в чем преимущества</li>
+          <li>Рыбий текст в чем преимущества</li>
+          <li>Рыбий текст в чем преимущества</li>
+          <li>Рыбий текст в чем преимущества</li>
+        </ul>
+      </div>
+      <div class="servises--btn btn">Подробнее</div>
+    </div>
+    <div class="servises--item--right">
+      <div class="servises--item--img"></div>
+    </div>
+  </div>
+  <!----------------------------------------------->
+  <div class="black-box-allservis">
+    <div class="black-box-allservis--left">
+    </div>
+    <div class="black-box-allservis--right">
+      <p class="black-box-allservis--font">РЫБИЙ ТЕКСТ РАССКАЗЫВАЮЩИЙ ПРО НАРАЩИВАНИЕ РЕСНИЦ. РЫБИЙ ТЕКСТ РАССКАЗЫВАЮЩИЙ ПРО НАРАЩИВАНИЕ РЕСНИЦ. РЫБИЙ ТЕКСТ РАССКАЗЫВАЮЩИЙ ПРО НАРАЩИВАНИЕ РЕСНИЦ. РЫБИЙ ТЕКСТ РАССКАЗЫВАЮЩИЙ ПРО НАРАЩИВАНИЕ РЕСНИЦ. РЫБИЙ ТЕКСТ РАССКАЗЫВАЮЩИЙ ПРО НАРАЩИВАНИЕ РЕСНИЦ. РЫБИЙ ТЕКСТ РАССКАЗЫВАЮЩИЙ ПРО НАРАЩИВАНИЕ РЕСНИЦ. </p>
+      <div class="black-box-allservis--btn btn">Все услуги</div>
+    </div>
+  </div>
+  <!----------------------------------------------->
+  <div class="office-box container">
+    <h2 class="h2-item">Забота и удобство в каждом моменте</h2>
+
+
+    <div class="office-box-slider--more">
+      <div class="office-box-slider">
+        <div class="office-box-sliderline">
+          <div class="office-box-sliderline--item"></div>
+          <div class="office-box-sliderline--item"></div>
+          <div class="office-box-sliderline--item"></div>
+        </div>
+        <div class="office-box-slider--left"></div>
+        <div class="office-box-slider--right"></div>
+      </div>
+    </div>
+
+
+    <div class="office-box-bestimg">
+      <div class="office-box-bestimg--item"></div>
+      <div class="office-box-bestimg--item"></div>
+      <div class="office-box-bestimg--item"></div>
+    </div>
+    <div class="office-box-officebenefits">
+      <ul class="office-box-officebenefits--spisok">
+        <li class="office-box-officebenefits--item">РЫБИЙ ТЕКСТ РАССКАЗЫАЮЩИЙ О ПРЕИМУЩЕСТВАХ</li>
+        <li class="office-box-officebenefits--item">РЫБИЙ ТЕКСТ РАССКАЗЫАЮЩИЙ О ПРЕИМУЩЕСТВАХ</li>
+        <li class="office-box-officebenefits--item">РЫБИЙ ТЕКСТ РАССКАЗЫАЮЩИЙ О ПРЕИМУЩЕСТВАХ</li>
+        <li class="office-box-officebenefits--item">РЫБИЙ ТЕКСТ РАССКАЗЫАЮЩИЙ О ПРЕИМУЩЕСТВАХ</li>
+        <li class="office-box-officebenefits--item">РЫБИЙ ТЕКСТ РАССКАЗЫАЮЩИЙ О ПРЕИМУЩЕСТВАХ</li>
+        <li class="office-box-officebenefits--item">РЫБИЙ ТЕКСТ РАССКАЗЫАЮЩИЙ О ПРЕИМУЩЕСТВАХ</li>
+      </ul>
+    </div>
+  </div>
+  <!----------------------------------------------->
+  <div class="black-box-freesnyat">
+    <div class="black-box-freesnyat--left">
+      <p class="black-box-freesnyat--font">РЫБИЙ ТЕКСТ РАССКАЗЫВАЮЩИЙ ПРО НАРАЩИВАНИЕ РЕСНИЦ. РЫБИЙ ТЕКСТ РАССКАЗЫВАЮЩИЙ ПРО НАРАЩИВАНИЕ РЕСНИЦ. РЫБИЙ ТЕКСТ РАССКАЗЫВАЮЩИЙ ПРО НАРАЩИВАНИЕ РЕСНИЦ. РЫБИЙ ТЕКСТ РАССКАЗЫВАЮЩИЙ ПРО НАРАЩИВАНИЕ РЕСНИЦ. РЫБИЙ ТЕКСТ РАССКАЗЫВАЮЩИЙ ПРО НАРАЩИВАНИЕ РЕСНИЦ. РЫБИЙ ТЕКСТ РАССКАЗЫВАЮЩИЙ ПРО НАРАЩИВАНИЕ РЕСНИЦ. </p>
+      <div class="black-box-freesnyat--btn btn">Все услуги</div>
+    </div>
+    <div class="black-box-freesnyat--right">
+    </div>
+  </div>
+  <!----------------------------------------------->
+  <div class="portfolio-box container">
+    <h2 class="h2-item">Последние выполненные работы</h2>
+    <div class="portfolio-box-greed">
+      <div class="portfolio-box-greed--item"></div>
+      <div class="portfolio-box-greed--item"></div>
+      <div class="portfolio-box-greed--item"></div>
+      <div class="portfolio-box-greed--item"></div>
+      <div class="portfolio-box-greed--item"></div>
+      <div class="portfolio-box-greed--item"></div>
+      <div class="portfolio-box-greed--item"></div>
+      <div class="portfolio-box-greed--item"></div>
+      <div class="portfolio-box-greed--item"></div>
+    </div>
+  </div>
+  <!----------------------------------------------->
+  <div class="black-box--masters container">
+    <h2 class="h2-item">Ведущие мастера Тюмени</h2>
+  </div>
+  <!----------------------------------------------->
+  <div class="reviews-box container">
+    <h2 class="h2-item">Отзывы</h2>
+  </div>
+  <!----------------------------------------------->
+  <div class="entryonline-box container">
+    <h2 class="h2-item">Записаться on-line</h2>
+  </div>
+  <!----------------------------------------------->
+  <div class="black-box container">
+    <h2 class="h2-item">Заголовок H2</h2>
+  </div>
+  <!----------------------------------------------->
+  <div class="faq-box container">
+    <h2 class="h2-item">Ответы на частые вопросы</h2>
+  </div>
+  <!----------------------------------------------->
+  <div class="contacts-box container">
+    <h2 class="h2-item">Контакты</h2>
+  </div>
+  <!----------------------------------------------->
 
 </body>
 
@@ -57,4 +223,3 @@ require '../mascara/templates/head.html';
 /* Footer */
 require '../mascara/templates/footer.html';
 ?>
-

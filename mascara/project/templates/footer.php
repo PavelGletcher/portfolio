40bc0888713b639617aef14f4acb6fff6c89a336
Павел Струначев <pavelstrunacev@iMac-Pavel.IGD_Rostelecom>
2023-07-31 15:47:09 +0500

    all clear

---
+++ mascara/project/templates/footer.php
@@ -0,0 +1,10 @@
+<footer>
+<div class="box--footer container">
+  
+</div>
+  <?php
+/* link js */
+require 'linkjs.html';
+?>
+</footer>
+</html>
---
+++ mascara/project/templates/head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru" oncopy="return false"; oncontextmenu="return true" itemscope itemtype="https://schema.org/FAQPage">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php
+/* favicon */
+require 'favicon.html';
+/* meta */
+require 'meta.html';
+/* css */
+require 'linkcss.html';
+?>
